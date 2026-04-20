77a2941b90cbb5b9dcfdbc34c158d87c2ebc3545
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-04-20 11:06:14 +0200

    build: set up gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    java
+    scala
+    application
+}
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.8")
+    implementation("com.omertron:API-OMDB:1.5")
+    implementation("org.jooq:jool:0.9.15")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.32")
+}
+
